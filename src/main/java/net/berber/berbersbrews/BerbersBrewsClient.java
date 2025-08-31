9ece167bac6942a68acbed99d9c0429976f9370a
berberborscing <zane.young@live.com>
2025-08-31 14:04:54 -0600

    - Fixed a glitch with the Ability Potion where scaling down a wall did not reset fall damage - Added doublejumps to the Ability Potion, number of doublejumps depends on potion effect amplifier - Updated sources so it doesn't lead to 404 page

--- src/main/java/net/berber/berbersbrews/BerbersBrewsClient.java
+++ src/main/java/net/berber/berbersbrews/BerbersBrewsClient.java
@@ -5,11 +5,13 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class BerbersBrewsClient implements ClientModInitializer {
     public static boolean isJumping = false;
+    public static boolean hasJumped = false;
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (client.player != null) {
                 isJumping = client.options.jumpKey.isPressed();
+                hasJumped = client.options.jumpKey.wasPressed();
             }
         });
     }
--- src/main/java/net/berber/berbersbrews/effect/AbleEffect.java
+++ src/main/java/net/berber/berbersbrews/effect/AbleEffect.java
@@ -10,19 +10,38 @@ public class AbleEffect extends StatusEffect {
     public AbleEffect(StatusEffectCategory statusEffectCategory, int color) {
         super(statusEffectCategory, color);
     }
+    int jumps = 0;
+    int jumpCooldown = 4;
 
     @Override
     public boolean applyUpdateEffect(LivingEntity entity, int amplifier) {
         if(entity instanceof PlayerEntity && entity.getWorld().isClient) {
+            if(entity.isOnGround()) {
+                jumps = amplifier + 1; //Reset # of jumps if player is on the ground
+                jumpCooldown = 4;
+            } else {
+                if(jumpCooldown > 0)
+                    jumpCooldown--;
+            }
             if(entity.horizontalCollision && BerbersBrewsClient.isJumping) {
+                jumpCooldown = 10;
+                entity.fallDistance = 0f;
                 if(entity.isSneaking()) {
-                    entity.setVelocity(entity.getVelocity().getX(), 0, entity.getVelocity().getZ());
+                    //0.003 is the smallest positive value that resets fall damage properly
+                    entity.setVelocity(entity.getVelocity().getX(), 0.003, entity.getVelocity().getZ());
                 } else {
                     entity.setVelocity(entity.getVelocity().getX(), 0.4, entity.getVelocity().getZ());
                 }
             }
+            else if(!entity.isOnGround() && !entity.horizontalCollision && BerbersBrewsClient.hasJumped && jumps > 0) {
+                if(jumpCooldown == 0) {
+                    jumps--; //Consume a jump
+                    entity.setVelocity(entity.getVelocity().getX(), 0.6, entity.getVelocity().getZ()); //Apply upward velocity
+                    entity.fallDistance = 0f;
+                    jumpCooldown = 4;
+                }
+            }
         }
-
         return true;
     }
 
